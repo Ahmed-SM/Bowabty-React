db08d2e5516a218a098580057bde29b3d0530cec
ahmed saadawi <f_22w3@hotmail.com>
2020-06-06 12:26:31 +0400

    MockData

---
+++ src/services/MockData.js
@@ -0,0 +1,69 @@
+import artBag from "../assets/serviceImgs/art-bag.png";
+import desk from "../assets/serviceImgs/desk.png";
+import docBlue from "../assets/serviceImgs/doc-blue.png";
+import docDarkGreen from "../assets/serviceImgs/doc-dark-green.png";
+import dockGreen from "../assets/serviceImgs/doc-green.png";
+import dockYellow from "../assets/serviceImgs/doc-yellow.png";
+import paperTime from "../assets/serviceImgs/paper-time.png";
+export const servicesList = [
+    {
+        Title: 'حالة طلباتي',
+        Description: 'تشمل طلبات القرطاسية صيانة المكتب والمعدات الأخرى في المكتب..',
+        Icon: paperTime
+    }, {
+        Title: 'طلبات متنوعة',
+        Description: 'تشمل طلبات القرطاسية صيانة المكتب والمعدات الأخرى في المكتب..',
+        Icon: artBag
+    }, {
+        Title: 'طلبات من مكتب التطوير المؤسسي',
+        Description: '',
+        Icon: dockGreen
+    }, {
+        Title: 'طلبات من قسم الاتصال المؤسسي',
+        Description: '',
+        Icon: docDarkGreen
+    }, {
+        Title: ' طلبات من قسم الموارد البشرية',
+        Description: '',
+        Icon: dockYellow
+    }, {
+        Title: 'طلبات من قسم الخدمات الإدارية',
+        Description: '',
+        Icon: docBlue
+    }, {
+        Title: 'مكتبي',
+        Description: ' تشمل طلبات القرطاسية صيانة المكتب والمعدات الأخرى في المكتب..',
+        Icon: desk
+    }
+]
+export const Newslist = [
+    {
+        header: 'اليوم العالمي للعدالة الاجتماعية',
+        Paragraph: ' لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكلوليس المحتوى) ويُستخدم ' +
+                'في صناعات المطابع ودور النشر.',
+        PostDate: 'الأربعاء 20 , فبراير 2019'
+    }, {
+        header: 'اليوم العالمي للعدالة الاجتماعية',
+        Paragraph: ' لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكلوليس المحتوى) ويُستخدم ' +
+                'في صناعات المطابع ودور النشر.',
+        PostDate: 'الأربعاء 20 , فبراير 2019'
+    }, 
+    {
+      header: 'اليوم العالمي للعدالة الاجتماعية',
+      Paragraph: ' لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكلوليس المحتوى) ويُستخدم ' +
+              'في صناعات المطابع ودور النشر.',
+      PostDate: 'الأربعاء 20 , فبراير 2019'
+  },
+  {
+    header: 'اليوم العالمي للعدالة الاجتماعية',
+    Paragraph: ' لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكلوليس المحتوى) ويُستخدم ' +
+            'في صناعات المطابع ودور النشر.',
+    PostDate: 'الأربعاء 20 , فبراير 2019'
+},
+ {
+    header: 'اليوم العالمي للعدالة الاجتماعية',
+    Paragraph: ' لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكلوليس المحتوى) ويُستخدم ' +
+            'في صناعات المطابع ودور النشر.',
+    PostDate: 'الأربعاء 20 , فبراير 2019'
+}
+]
