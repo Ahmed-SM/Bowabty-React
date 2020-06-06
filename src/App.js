eaa19a8fa851cb6c237704f7dbbf32deb89793f2
ahmed saadawi <f_22w3@hotmail.com>
2020-06-06 12:26:06 +0400

    Localization

--- src/App.js
+++ src/App.js
@@ -1,88 +1,19 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import MainLayout from './layouts/mainLayout'
 import {Route} from "react-router-dom";
 import MyService from "./views/MyService/myService";
+import {ServiceContext, NewsFeedContext} from "./contexts/ServiceContext";
+import {DiractionProvider} from "./contexts/DiractionContext";
+import {servicesList, Newslist} from './services/MockData';
 import './App.css';
-import artBag from "./assets/serviceImgs/art-bag.png";
-import desk from "./assets/serviceImgs/desk.png";
-import docBlue from "./assets/serviceImgs/doc-blue.png";
-import docDarkGreen from "./assets/serviceImgs/doc-dark-green.png";
-import dockGreen from "./assets/serviceImgs/doc-green.png";
-import dockYellow from "./assets/serviceImgs/doc-yellow.png";
-import paperTime from "./assets/serviceImgs/paper-time.png";
-import {ServiceContext, LocalsContext, NewsFeedContext} from "./contexts/ServiceContext";
-
-const servicesList = [
-    {
-        Title: 'حالة طلباتي',
-        Description: 'تشمل طلبات القرطاسية صيانة المكتب والمعدات الأخرى في المكتب..',
-        Icon: paperTime
-    }, {
-        Title: 'طلبات متنوعة',
-        Description: 'تشمل طلبات القرطاسية صيانة المكتب والمعدات الأخرى في المكتب..',
-        Icon: artBag
-    }, {
-        Title: 'طلبات من مكتب التطوير المؤسسي',
-        Description: '',
-        Icon: dockGreen
-    }, {
-        Title: 'طلبات من قسم الاتصال المؤسسي',
-        Description: '',
-        Icon: docDarkGreen
-    }, {
-        Title: ' طلبات من قسم الموارد البشرية',
-        Description: '',
-        Icon: dockYellow
-    }, {
-        Title: 'طلبات من قسم الخدمات الإدارية',
-        Description: '',
-        Icon: docBlue
-    }, {
-        Title: 'مكتبي',
-        Description: ' تشمل طلبات القرطاسية صيانة المكتب والمعدات الأخرى في المكتب..',
-        Icon: desk
-    }
-]
-const Newslist = [
-    {
-        header: 'اليوم العالمي للعدالة الاجتماعية',
-        Paragraph: ' لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكلوليس المحتوى) ويُستخدم ' +
-                'في صناعات المطابع ودور النشر.',
-        PostDate: 'الأربعاء 20 , فبراير 2019'
-    }, {
-        header: 'اليوم العالمي للعدالة الاجتماعية',
-        Paragraph: ' لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكلوليس المحتوى) ويُستخدم ' +
-                'في صناعات المطابع ودور النشر.',
-        PostDate: 'الأربعاء 20 , فبراير 2019'
-    }, 
-    {
-      header: 'اليوم العالمي للعدالة الاجتماعية',
-      Paragraph: ' لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكلوليس المحتوى) ويُستخدم ' +
-              'في صناعات المطابع ودور النشر.',
-      PostDate: 'الأربعاء 20 , فبراير 2019'
-  },
-  {
-    header: 'اليوم العالمي للعدالة الاجتماعية',
-    Paragraph: ' لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكلوليس المحتوى) ويُستخدم ' +
-            'في صناعات المطابع ودور النشر.',
-    PostDate: 'الأربعاء 20 , فبراير 2019'
-},
- {
-    header: 'اليوم العالمي للعدالة الاجتماعية',
-    Paragraph: ' لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكلوليس المحتوى) ويُستخدم ' +
-            'في صناعات المطابع ودور النشر.',
-    PostDate: 'الأربعاء 20 , فبراير 2019'
-}
-]
-
-const local = {
-    isLTR: false
-}
+import "./i18n";
 
 function App() {
+    console.log("App rerendred")
     return (
+        <Suspense fallback={<div>Loading..</div>}>
         <ServiceContext.Provider value={servicesList}>
-            <LocalsContext.Provider value={local}>
+            <DiractionProvider>
                 <MainLayout>
                     {/* Dynamic Components are passed down to the Layout,
                      The Layout component also wraps up a essential static components*/}
@@ -90,8 +21,9 @@ function App() {
                         <Route exact path="/" component={MyService}/>
                     </NewsFeedContext.Provider>
                 </MainLayout>
-            </LocalsContext.Provider>
+            </DiractionProvider>
         </ServiceContext.Provider>
+        </Suspense>
     );
 }
 
