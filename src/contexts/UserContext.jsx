34f1bef30397f654fbd498f0876f0fe7a64b9c90
ahmed saadawi <f_22w3@hotmail.com>
2020-06-10 04:32:31 +0400

    User Context comment

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,7 +1,7 @@
 import React, { useState, createContext } from 'react';
 const UserContext = createContext([{}, () => {}]);
 const UserProvider  = ({children}) => {
-    const [userData, setUserData] = useState(false);
+    const [userData, setUserData] = useState(true); // 📌 LOGIN: change this to false to enable Login 📌
     return (
       <UserContext.Provider value={[userData, setUserData]}>
         {children}
