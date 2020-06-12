3e3dfafe3bfe455552beea0b9f384b1a9a597c0a
ahmed saadawi <f_22w3@hotmail.com>
2020-06-12 13:35:44 +0400

    common file

--- src/layouts/mainLayout.jsx
+++ src/layouts/mainLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import {Header} from "./components";
-import {MainNav} from "./components";
+import {Header} from "./common";
+import {MainNav} from "./common";
 
 
 const mainLayout = ({children}) => {
