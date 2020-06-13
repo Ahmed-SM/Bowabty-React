5297a8b92c3d4b72d18dc6156bed140ad5861091
ahmed saadawi <f_22w3@hotmail.com>
2020-06-13 17:54:15 +0400

    Custom inputs

---
+++ src/components/CustomInputs/index.jsx
@@ -0,0 +1,117 @@
+import React from "react";
+import styled from "styled-components";
+import {useField} from "formik";
+
+export const CustomInput = React.memo(({ label, ...props }) => {
+  console.log("%c CustomInput rerendred ", "background:red; color: white;");
+    const [field, meta] = useField(props);
+    return (
+      <>
+        <label htmlFor={props.id || props.name}>{label}</label>
+        <StyledInput {...field} {...props} />
+        {meta.touched && meta.error ? (
+          <div className="error">{meta.error}</div>
+        ) : null}
+      </>
+    );
+  });
+
+export const CustomSelect = React.memo(({ label, ...props }) => {
+  console.log("%c CustomSelect rerendred ", "background:red; color: white;");
+    const [field, meta] = useField(props);
+    return (
+      <>
+        <label htmlFor={props.id || props.name}>{label}</label>
+        <StyledSelect {...field} {...props} />
+        {meta.touched && meta.error ? (
+          <div className="error">{meta.error}</div>
+        ) : null}
+      </>
+    );
+});
+export const CustomFile = React.memo(({ label, ...props }) => {
+  console.log("%c CustomSelect rerendred ", "background:red; color: white;");
+    const [field, meta] = useField(props);
+    return (
+      <>
+        <label htmlFor={props.id || props.name}>{label}</label>
+        <StyledFile {...field} {...props}/>
+        {meta.touched && meta.error ? (
+          <div className="error">{meta.error}</div>
+        ) : null}
+      </>
+    );
+});
+
+const StyledInput = styled.input`
+ height: 3rem;
+  min-height: 3rem;
+  margin-bottom: 0.1rem;
+  border-color: black;
+  width: 100%;
+  color: #29333f;
+  font-size: 1.3rem;
+  outline: 0;
+  transition: all 0.3s ease-in-out;
+  min-height: 30px;
+  height: 30px;
+  border-radius: 5px;
+  text-rendering: auto;
+  letter-spacing: normal;
+  word-spacing: normal;
+  text-indent: 15px;
+  text-shadow: none;
+  display: inline-block;
+  text-align: start;
+  align-items: center;
+  white-space: pre;
+  -webkit-rtl-ordering: logical;
+  cursor: pointer;
+  border-width: 1px;
+  border-style: solid;
+  border-color: rgb(169, 169, 169);
+`;
+const StyledSelect = styled.select`
+    min-height: 30px;
+    border-radius: 5px;
+    text-rendering: auto;
+    letter-spacing: normal;
+    word-spacing: normal;
+    text-indent: 15px;
+    text-shadow: none;
+    display: inline-block;
+    text-align: start;
+    align-items: center;
+    white-space: pre;
+    rtl-ordering: logical;
+    cursor: pointer;
+    border-width: 1px;
+    border-style: solid;
+    border-color: rgb(169, 169, 169);
+`;
+const StyledFile = styled.input`
+    height: 30px;
+    min-height: 30px;
+    border-radius: 5px;
+    text-rendering: auto;
+    letter-spacing: normal;
+    word-spacing: normal;
+    text-indent: 0px;
+    text-shadow: none;
+    display: inline-block;
+    text-align: start;
+    align-items: center;
+    white-space: pre;
+    rtl-ordering: logical;
+    cursor: pointer;
+    border-width: 1px;
+    border-style: solid;
+    border-color: rgb(169, 169, 169);
+    display: flex;
+    flex-direction: row-reverse;
+    background-color: transparent;
+    & input[type=file] {
+      font-size: calc(1px + 0.65vw);
+      opacity: 0;
+  }
+`;
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,10 @@
+export {  CustomInput } from './CustomInputs';
+export {  CustomSelect } from './CustomInputs';
+// export {  CustomFile } from './CustomInputs'; //not ready yet
 export { default as MyOrders } from './MyOrders';
 export { default as IncomingRequest } from './IncomingRequest';
 export { default as MyService } from './MyService';
 export { default as ServiceSlider } from './ServiceSlider';
-export { default as CustomInput } from './CustomInput';
 export { default as StickyBox } from './StickyLabel';
 export { default as Login } from './Login';
 export { default as UserSection } from './UserSection';
