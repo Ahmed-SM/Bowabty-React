e5ad5e924382843823c8cce0799af27203f8e37a
ahmed saadawi <f_22w3@hotmail.com>
2020-06-15 10:09:27 +0400

    ...

--- src/components/ReusableBoxes/MediumBox.jsx
+++ src/components/ReusableBoxes/MediumBox.jsx
@@ -1,6 +1,6 @@
 import React, {useContext} from "react";
 import styled from "styled-components";
-import '../ServiceSlider/node_modules/react-multi-carousel/lib/styles.css';
+import 'react-multi-carousel/lib/styles.css';
 import {DiractionContext} from "../../contexts/DiractionContext";
 
 
