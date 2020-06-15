c8f4d827df94c22030556392e402582064293289
ahmed saadawi <f_22w3@hotmail.com>
2020-06-15 04:38:53 +0400

    .

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import {UserProvider} from "./contexts/UserContext";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
