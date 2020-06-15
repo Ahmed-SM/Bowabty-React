4a7906bf53154770a4d2ddf8bc30ae91a728b301
ahmed saadawi <f_22w3@hotmail.com>
2020-06-15 10:02:08 +0400

    ..

--- src/components/ReusableBoxes/MediumBox.jsx
+++ src/components/ReusableBoxes/MediumBox.jsx
@@ -1,6 +1,6 @@
 import React, {useContext} from "react";
 import styled from "styled-components";
-import '../serviceSlider/node_modules/react-multi-carousel/lib/styles.css';
+import '../ServiceSlider/node_modules/react-multi-carousel/lib/styles.css';
 import {DiractionContext} from "../../contexts/DiractionContext";
 
 
