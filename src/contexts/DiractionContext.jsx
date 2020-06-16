dab0b082908445e67db0addf8a9873f800c35dc3
ahmed saadawi <f_22w3@hotmail.com>
2020-06-16 10:28:29 +0400

    .

--- src/contexts/DiractionContext.jsx
+++ src/contexts/DiractionContext.jsx
@@ -1,11 +1,11 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext } from "react";
 const DiractionContext = createContext([{}, () => {}]);
-const DiractionProvider  = ({children}) => {
-    const [isLTR, setIsLTR] = useState(false);
-    return (
-      <DiractionContext.Provider value={[isLTR, setIsLTR]}>
-        {children}
-      </DiractionContext.Provider>
-    );
-  }
-  export { DiractionContext, DiractionProvider };
+const DiractionProvider = ({ children }) => {
+  const [isLTR, setIsLTR] = useState(false);
+  return (
+    <DiractionContext.Provider value={[isLTR, setIsLTR]}>
+      {children}
+    </DiractionContext.Provider>
+  );
+};
+export { DiractionContext, DiractionProvider };
