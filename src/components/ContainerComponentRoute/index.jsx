2e8d8aea8c5c22e0b02c7ff79caa105c507eb89b
ahmed saadawi <f_22w3@hotmail.com>
2020-06-17 01:11:17 +0400

    fallback

--- src/components/ContainerComponentRoute/index.jsx
+++ src/components/ContainerComponentRoute/index.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-const ContainerComponentRoute = ({ container: Container, component: Component, children, fallback:Fallback, ...rest }) => {
+const ContainerComponentRoute = ({ container: Container, component: Component, ...rest }) => {
   console.log("%c ContainerComponentRoute rerendred ", "background:green; color: white;");
   return (
     <Route
       {...rest}
       render={props  => (
-        <React.Suspense fallback={Fallback}>
-          <Container>
-            <Component {...props}/>
-          </Container>
-        </React.Suspense>
+        <Container>
+          <Component {...props}/>
+        </Container>
       )}
     />
   );
@@ -21,7 +19,6 @@ const ContainerComponentRoute = ({ container: Container, component: Component, c
 ContainerComponentRoute.propTypes = {
   component: PropTypes.any.isRequired,
   container: PropTypes.any.isRequired,
-  fallback:PropTypes.any.isRequired
 };
 
 export default ContainerComponentRoute;
