6ce766d8e6087ad8d56f6ffc8647e336feea3690
ahmed saadawi <f_22w3@hotmail.com>
2020-06-21 08:15:36 +0400

    custom file

--- src/components/CustomInputs/index.jsx
+++ src/components/CustomInputs/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { useField } from "formik";
+import { Field, useField } from "formik";
 
 export const CustomInput = React.memo(({ label, ...props }) => {
   console.log("%c CustomInput rerendred ", "background:red; color: white;");
@@ -16,13 +16,13 @@ export const CustomInput = React.memo(({ label, ...props }) => {
   );
 });
 
-export const CustomSelect = React.memo(({ label, ...props }) => {
+export const CustomSelect = React.memo(({ label, type, ...props }) => {
   console.log("%c CustomSelect rerendred ", "background:red; color: white;");
   const [field, meta] = useField(props);
   return (
     <React.Fragment>
       <label htmlFor={props.id || props.name}>{label}</label>
-      <StyledSelect {...field} {...props} />
+      <StyledSelect as={type} {...field} {...props} />
       {meta.touched && meta.error ? (
         <div className="error">{meta.error}</div>
       ) : null}
@@ -34,27 +34,35 @@ export const CustomFile = React.memo(({ label, ...props }) => {
   const [field, meta] = useField(props);
   return (
     <React.Fragment>
-      <label htmlFor={props.id || props.name}>{label}</label>
-      <StyledFile {...field} {...props} />
+      <label className="custom-file-upload" htmlFor={props.id || props.name}>{label}</label>
+      <StyledFile type="file" {...field} {...props} />
       {meta.touched && meta.error ? (
         <div className="error">{meta.error}</div>
       ) : null}
     </React.Fragment>
   );
 });
+export const CustomRadio= React.memo(({ label, ...props }) => {
+  console.log("%c CustomRadio rerendred ", "background:red; color: white;");
+  const [field, meta] = useField(props);
+  return (
+    <React.Fragment>
+      <StyledRadio {...field} {...props}></StyledRadio>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+        ) : null}
+    </React.Fragment>
+  );
+});
 
-const StyledInput = styled.input`
-  height: 3rem;
+const StyledInput = styled(Field)`
   min-height: 3rem;
   margin-bottom: 0.1rem;
   border-color: black;
   width: 100%;
   color: #29333f;
   font-size: 1.3rem;
-  outline: 0;
-  transition: all 0.3s ease-in-out;
-  min-height: 30px;
-  height: 30px;
   border-radius: 5px;
   text-rendering: auto;
   letter-spacing: normal;
@@ -64,15 +72,17 @@ const StyledInput = styled.input`
   display: inline-block;
   text-align: start;
   align-items: center;
-  white-space: pre;
-  -webkit-rtl-ordering: logical;
+  white-space: break-spaces;
   cursor: pointer;
   border-width: 1px;
   border-style: solid;
   border-color: rgb(169, 169, 169);
+  option{
+    font-size: 1.3rem;
+  }
 `;
 const StyledSelect = styled.select`
-  min-height: 30px;
+  min-height: 3rem;
   border-radius: 5px;
   text-rendering: auto;
   letter-spacing: normal;
@@ -83,35 +93,29 @@ const StyledSelect = styled.select`
   text-align: start;
   align-items: center;
   white-space: pre;
-  rtl-ordering: logical;
   cursor: pointer;
   border-width: 1px;
   border-style: solid;
   border-color: rgb(169, 169, 169);
+
+  option{
+    font-size: 1.3rem;
+  }
 `;
 const StyledFile = styled.input`
-  height: 30px;
-  min-height: 30px;
+  display: none !important;
+`;
+const StyledRadio = styled.input`
+  
   border-radius: 5px;
-  text-rendering: auto;
-  letter-spacing: normal;
-  word-spacing: normal;
-  text-indent: 0px;
-  text-shadow: none;
   display: inline-block;
   text-align: start;
   align-items: center;
   white-space: pre;
-  rtl-ordering: logical;
   cursor: pointer;
   border-width: 1px;
   border-style: solid;
   border-color: rgb(169, 169, 169);
-  display: flex;
-  flex-direction: row-reverse;
-  background-color: transparent;
-  & input[type="file"] {
-    font-size: calc(1px + 0.65vw);
-    opacity: 0;
-  }
+  width:3.5rem;
+
 `;
