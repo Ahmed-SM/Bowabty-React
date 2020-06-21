44d70efee1a61f70e9f53ff4305e68e8f615bd58
ahmed saadawi <f_22w3@hotmail.com>
2020-06-21 08:09:12 +0400

    boxes colors

--- src/components/ReusableBoxes/LargeBox.jsx
+++ src/components/ReusableBoxes/LargeBox.jsx
@@ -16,12 +16,15 @@ export default LargeBox;
 
 const StyledInner = styled.div`
   height: 560px;
-  min-height: 540px;
   padding: 20px 20px 0 20px;
   margin-top: 40px;
   box-shadow: 0px 0px 8px 3px rgba(0, 0, 0, 0.09);
   background-color: white;
   border-radius: 15px;
   z-index: 0;
-  border-bottom: 6px solid ${(props) => (props.primary ? "#3497DB" : "#000000")};
+  ${({ isLTR }) =>
+      isLTR &&
+      ` direction: ltr;
+        `}
+  border-bottom: 6px solid ${(props) => (props.primary ? "#3497DB" : props.green ? "#008B38" : props.lightgreen ? "#1BBC9B" : props.purple ? "#902577" : "#000000")};
 `;
--- src/components/ReusableBoxes/MediumBox.jsx
+++ src/components/ReusableBoxes/MediumBox.jsx
@@ -17,6 +17,7 @@ export default MediumBox;
 const StyledInner = styled.div`
     height: 320px;
     margin: 20px auto;
+    padding: 20px 50px 0px 50px;
     width: 46%;
     min-width: 46%;
     max-width:50%;
@@ -29,6 +30,6 @@ const StyledInner = styled.div`
       isLTR &&
       ` direction: ltr;
         `}
-    border-bottom: 6px solid ${(props) =>
-      props.primary ? "#3497DB" : "#000000"};
+        border-bottom: 6px solid ${(props) => (props.primary ? "#3497DB" : props.green ? "#008B38" : props.lightgreen ? "#1BBC9B" : props.purple ? "#902577" : "#000000")};
+        
   `;
